c2f0ed41a415582d255bd13a418eaa2ae2e6837b
ZiRoberto <ereneren326@gmail.com>
2025-01-26 04:33:24 +0000

    fix: layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,11 @@
 import Logo from '@/assets/Logo';
 import React from 'react';
+import Image from 'next/image';
 
 const Footer = () => {
   return (
     <div className="flex flex-col gap-3 w-full pt-6 max-md:pt-4 border-t border-white border-opacity-10 justify-center items-center">
-      <Logo width={75} height={34} />
+      <Image src="/images/f-logo.png" alt="logo" width={50} height={45} />
       <p className="text-[10px] font-normal leading-3 text-center text-secondary">
         © 2025 FRIDAY AI. All rights reserved.
       </p>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,11 +5,12 @@ import TwitterIcon from '@/assets/TwitterIcon';
 import TelegramIcon from '@/assets/TelegramIcon';
 import GitHubIcon from '@/assets/GitHubIcon';
 import DexscreenerIcon from '@/assets/DexscreenerIcon';
+import Image from 'next/image';
 
 const Navbar = () => {
   return (
     <div className="flex justify-between items-center">
-      <Logo width={75} height={34} />
+      <Image src="/images/logo.png" alt="logo" width={150} height={50} />
       <div className="flex items-center gap-2">
         <Link href="">
           <TwitterIcon />
@@ -17,7 +18,7 @@ const Navbar = () => {
         <Link href="">
           <TelegramIcon />
         </Link>
-        <Link href="">
+        <Link href="https://github.com/Friday-Al" about="_blank">
           <GitHubIcon />
         </Link>
         <Link href="">
