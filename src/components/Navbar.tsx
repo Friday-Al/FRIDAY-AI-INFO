8bb58292b87fb41b78b5fa59d57d561bb9979dc9
ZiRoberto <ereneren326@gmail.com>
2025-01-27 20:25:42 +0000

    fix: twitter link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,13 +11,13 @@ const Navbar = () => {
     <div className="flex justify-between items-center">
       <Image src="/images/logo.png" alt="logo" width={150} height={50} />
       <div className="flex items-center gap-2">
-        <Link href="">
+        <Link href="https://x.com/friday_dot_run" target="_blank">
           <TwitterIcon />
         </Link>
         <Link href="">
           <TelegramIcon />
         </Link>
-        <Link href="https://github.com/Friday-Al" about="_blank">
+        <Link href="https://github.com/Friday-Al" target="_blank">
           <GitHubIcon />
         </Link>
         <Link href="">
